58c7c6296fc7dd690eca10d173d1331ea64adc78
Alex Tsamakos <alex@tsamakos.com>
2017-02-08 15:51:33 +0100

    Refactor initLora()
    The refactoring includes spliting the method into ota/abp and using the bare init() method of the RN2483 library to simplify the code.

--- SodaqOneTracker_v2/Enums.h
+++ SodaqOneTracker_v2/Enums.h
@@ -7,4 +7,14 @@ enum LoraInitResult {
     LORA_INIT_DEVICE_ONLY
 };
 
+enum LoraInitConsoleMessages {
+    LORA_INIT_SKIP_CONSOLE_MESSAGES,
+    LORA_INIT_SHOW_CONSOLE_MESSAGES
+};
+
+enum LoraInitJoin {
+    LORA_INIT_SKIP_JOIN,
+    LORA_INIT_JOIN
+};
+
 #endif
